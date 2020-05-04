33c18457f940526f1ce0f4ec964c66ae22d63e65
frank <frank@starsriver.cn>
2020-05-04 16:39:04 +0800

    二进制1个数

--- interview/n14_2.go
+++ interview/n14_2.go
@@ -1,6 +1,10 @@
 /*
 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m] 。请问 k[0]*k[1]*...*k[m] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
 
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+
+
 示例 1：
 
 输入: 2
@@ -13,32 +17,23 @@
 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof
+链接：https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 package interview
 
-// func cuttingRope(n int) int {
-// 	dp := make([]int, n+1)
-// 	dp[0] = 0
-// 	dp[1] = 1
-// 	dp[2] = 1
+func cuttingRope(n int) int {
+	if n <= 3 {
+		return n - 1
+	}
 
-// 	for i := 3; i <= n; i++ {
-// 		max := 0
-// 		for j := 2; j < i; j++ {
-// 			if j*dp[i-j] > max {
-// 				max = j * dp[i-j]
-// 			}
+	res := int64(1)
+	p := (int64)(1e9 + 7)
+	for n > 4 {
+		res = res * 3 % p
+		n -= 3
+	}
 
-// 			if j*(i-j) > max {
-// 				max = j * (i - j)
-// 			}
-// 		}
-
-// 		dp[i] = max
-// 	}
-
-// 	return dp[n]
-// }
+	return int(res * int64(n) % p)
+}
--- interview/n15.go
+++ interview/n15.go
@@ -1,39 +1,28 @@
 /*
-给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m] 。请问 k[0]*k[1]*...*k[m] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
-
-答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
-
-
+请实现一个函数，输入一个整数，输出该数二进制表示中 1 的个数。例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
 
 示例 1：
 
-输入: 2
-输出: 1
-解释: 2 = 1 + 1, 1 × 1 = 1
-示例 2:
-
-输入: 10
-输出: 36
-解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+输入：00000000000000000000000000001011
+输出：3
+解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof
+链接：https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 package interview
 
-func cuttingRope(n int) int {
-	if n <= 3 {
-		return n - 1
+func hammingWeight(num uint32) int {
+	count := 0
+	for num != 0 {
+		if num%2 == 1 {
+			count++
+		}
+
+		num = num >> 1
 	}
 
-	res := int64(1)
-	p := (int64)(1e9 + 7)
-	for n > 4 {
-		res = res * 3 % p
-		n -= 3
-	}
-
-	return int(res * int64(n) % p)
+	return count
 }
